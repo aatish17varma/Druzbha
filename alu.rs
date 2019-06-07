46d18631e39f7758f9b20a04a7af1fd0e32baf56
Aatish Varma <aatish17varma@gmail.com>
2019-06-07 14:39:59 -0400

    removed packet.rs

--- alu.rs
+++ alu.rs
@@ -2,20 +2,31 @@
 
 
 //A Stateful ALU
-struct StatefulAlu{
-    stateful_variables: Vec<i32>,
-    sequential_function: Fn(vars -> vars),
-    // state_array:
+
+
+struct ALU(&mut Vec<i32> global_states){
+    states: &mut Vec<i32>, 
+    sequential_function: Box(fn(i32, Vec<i32>)) -> Vec<i32>),
+    is_stateful: bool
 }
 
 impl StatefulAlu{
-    fn operator(packet: &packet::packet){
-        sequential_function()
+    //Add a constructor with an intial value for is_stateful hard coded
+
+    fn operator(&self, packet: &packet::packet){
+        self.sequential_function()
     }
+
 }
 
 
 fn main(){
-    let stateVariables = 
+    let vec: &mut Vec<i32> = Vec::new();
+    
+    fn temp_function(){
+
+    }
+
+    let temp_ALU = ALU{states: vec, sequential_function}
 
 }
