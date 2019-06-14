f91020df5e3e9dc0a8b834d81119a6f9d76092ab
Aatish Varma <aatish17varma@gmail.com>
2019-06-14 17:14:49 -0400

    pipeline stage

--- pipeline_stage.rs
+++ pipeline_stage.rs
@@ -1,19 +1,41 @@
 // TODO: File representing a single pipeline stage
 
+mod alu;
+mod phv; 
+
+use rand::{thread_rng, Rng};
 
 pub struct Stage{
-    atoms: Vec<i32>
+    atoms: &mut Vec<alu::ALU>
 }
 
 impl Stage{
-    pub fn new() => {}
-    pub fn new_with_atom() => {}
-    pub fn tick() => {}
+
+    /*Ask Anirudh how neccesary it is to have an initializer list constructor.*/
+
+    pub fn new(vec_of_atoms : &mut Vec<alu::ALU>) -> Self {
+        return Stage{ atoms: vec_of_atoms };
+    }
+
+    pub fn tick(single_packet : phv::Phv, &self) -> phv::Phv {
+        if (single_packet.bubble){
+            return phv::Phv::new(); 
+        }
+        else{
+            let ret = phv::Phv::new();
+            //Randomly shuffle atom vector
+            thread_rng().shuffle(self.atoms);
+            for atom in self.atoms {
+                let current_phv = single_packet;
+                atom(current_phv);
+                ret += current_phv.clone();
+            }
+            return ret;
+        }
+    }
 }
 
 fn main(){
-
-
-
+    println!("hello world");
 }
 
