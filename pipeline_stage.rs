433bd35697ffa1e72dc138bb2bde5ba6dd50586c
Aatish Varma <aatish17varma@gmail.com>
2019-06-14 16:27:32 -0400

    beginning of pipeline_stage

--- pipeline_stage.rs
+++ pipeline_stage.rs
@@ -1,1 +1,19 @@
 // TODO: File representing a single pipeline stage
+
+
+pub struct Stage{
+    atoms: Vec<i32>
+}
+
+impl Stage{
+    pub fn new() => {}
+    pub fn new_with_atom() => {}
+    pub fn tick() => {}
+}
+
+fn main(){
+
+
+
+}
+
