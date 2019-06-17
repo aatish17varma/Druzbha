213eac2c8a909c38d9912fbc47fdce816b80a959
Aatish Varma <aatish17varma@gmail.com>
2019-06-17 13:57:26 -0400

    current pipelien

--- pipeline.rs
+++ pipeline.rs
@@ -1,1 +1,30 @@
 // TODO: File representing an entire pipeline
+// TODO: Work on this
+
+//use pipeline_stage::PipelineStage;
+
+struct Pipeline {
+ //  pipeline_stages : Vec<PipelineStage>,
+}
+// TODO: Test this once PipelineStage has been
+// defined
+impl Pipeline {
+    /*
+  pub fn new (t_stages : &Vec <PipelineStage>) -> Self {
+    Pipeline {stages : t_stages.clone() }
+  }
+
+  pub fn tick (&self, packet : Phv) -> Phv {
+    assert (self.stages.len() >0);
+    if (self.stages.len()==1){
+      self.stages[0].tick (packet)
+    }
+    for i..self.stages.len()-1{
+      self.stages[i].tick (packet);
+    }
+    self.stages[self.stages.len()-1].tick(packet)
+  }
+  */
+}
+
+
